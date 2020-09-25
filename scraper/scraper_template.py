baf010bab699558c7f08be05669e70b5c0fa63ae
MaxDistructo <MaxDistructo@users.noreply.github.com>
2020-09-25 13:57:44 -0600

    Update scraper_template.py

--- scraper/scraper_template.py
+++ scraper/scraper_template.py
@@ -10,19 +10,38 @@ def scrape_html() -> list:
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
     data = soup.findAll("tag", {"id": "id_name", "class": "class_name"}) # use any technique supported by BeautifulSoup to pull the data out.
-    # If using the default template, return in the format: Staff cases, Student cases, Total cases, Quarantined 
-    return ["", "", "", ""]
+    # If using the default template, return in the format: Staff cases, Student cases, Total cases, Quarantined, On Campus Quarantine
+    return ["", "", "", "", ""]
     
     
-def scrape_pdf() -> list:
+def scrape_pdf(online: bool) -> list:
   url = ""
-  filename = "./NAMETemp.pdf"
-  with urllib.request.urlopen(url) as response, open(filename, 'wb') as out_file:
-    shutil.copyfileobj(response, out_file)
+  filename = "./pdf/NAMETemp.pdf"
+  if online:
+    with urllib.request.urlopen(url) as response, open(filename, 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+  data = pdfToText(filename)
   # If using the default template, return in the format: Staff cases, Student cases, Total cases, Quarantined 
-  return ["", "", "", ""]
+  return ["", "", "", "", ""]
+
+def scrape_csv(online: bool) -> list:
+    url = "example.com"
+    filename = "./csv/NAMETemp.csv"
+    if online:
+        with urllib.request.urlopen(url) as response, open(filename, 'wb') as out_file:
+            shutil.copyfileobj(response, out_file)
+    data = {}
+    # Personal preference is to convert the CSV to dict. You can ignore this if you wish to write your own interpreter
+    with open(filename) as f:
+        reader = csv.reader(f, skipinitialspace=True, quotechar="'")
+        for row in reader:
+            data[row[0]] = row[1:]
+            
+    return data[element_name]
   
-# Below is a copy of a cleanup function that is commonly used. This will strip out all letters from the list elements. Empty elements are also stripped
+# All code below is provided in the main code and is here for reference. 
+
+# This will strip out all letters from the list elements. Empty elements are also stripped
 def strip_results(results: list) -> list:
     i = 0
     while i < len(results):
@@ -33,3 +52,17 @@ def strip_results(results: list) -> list:
         else:
             i += 1
     return results
+
+def pdfToText(string: str) -> list:
+    fd = open(string, "rb")
+    viewer = SimplePDFViewer(fd)
+
+    plain_text = []
+    try:
+        while True:
+            viewer.render()
+            plain_text += viewer.canvas.strings
+            viewer.next()
+    except PageDoesNotExist:
+        pass
+    return plain_text
